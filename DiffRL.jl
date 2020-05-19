4d411edbf910dacf3bb0265081d4989de8ca1702
S. Sahm <s.sahm@C02ZT2A6MD6R.local>
2020-05-19 08:26:41 +0200

    works now

--- DiffRL.jl
+++ DiffRL.jl
@@ -1,5 +1,25 @@
+
+#=
+# Description of the problem:
+    There is Trebuchet, which throws a mass to a target. The mass is to be
+    released at an angle, and at certain velocity so that it lands on the target.
+    The velocity of release is determined by the counterweight of the Trebuchet.
+    Given conditions of environment we are required to predict the angle of
+    release and counterweight.
+
+    The problem is gamified by introducing a threshold. The player gets 99
+    attempts. The first attempt has threshold value of 1. Threshold determines
+    the tolerable relative error between actual distance travelled by the Projectile
+    and the target distance. This relative error has to be less than the threshold
+    in order to continue playing the game. The game gets tougher at each attempts
+    by reduction of threshold by 0.01.
+
+# Input:  Wind speed,   Target distance
+# Output: ReleaseAngle, Weight
+=#
+
 using Flux, Trebuchet
-using Flux.Tracker: forwarddiff
+using Zygote: forwarddiff
 using Statistics: mean
 using Random
 
@@ -27,16 +47,10 @@ function aim(wind, target)
   angle, weight
 end
 
-distance(wind, target) =
-  shoot(Tracker.collect([wind, aim(wind, target)...]))
+distance(wind, target) = shoot(collect([wind, aim(wind, target)...]))
 
 function loss(wind, target)
-  try
     (distance(wind, target) - target)^2
-  catch e
-    # Roots.jl sometimes give convergence errors, ignore them
-    param(0)
-  end
 end
 
 DIST  = (20, 100)	# Maximum target distance
